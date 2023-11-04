e270aa8a85f4525feb4e4b64eb2bb01ed7a49594
hdh12191 <hdh12191@naver.com>
2023-11-05 03:17:20 +0900

    스타벅스 페이지 만들기 마무리

--- 231103/js/main.js
+++ 231103/js/main.js
@@ -18,6 +18,7 @@ searchInputEl.addEventListener("blur", function () {
 });
 
 const badgeEl = document.querySelector("header .badges");
+const toTopEl = document.querySelector("#to-top");
 
 window.addEventListener(
   "scroll",
@@ -30,15 +31,29 @@ window.addEventListener(
         opacity: 0,
         display: "none",
       });
+      // 버튼 보이기!
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
       //배지 보이기
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: "block",
       });
+      //버튼 숨기기!
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
     }
   }, 300)
 );
+
+toTopEl.addEventListener("click", function () {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
 
 const fadeEls = document.querySelectorAll(".visual .fade-in");
 fadeEls.forEach(function (fadeEl, index) {
@@ -72,6 +87,17 @@ new Swiper(".promotion .swiper", {
   },
 });
 
+new Swiper(".awards .swiper", {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: ".awards .swiper-prev",
+    nextEl: ".awards .swiper-next",
+  },
+});
+
 const promotionEl = document.querySelector(".promotion");
 const promotionToggleBtn = document.querySelector(".toggle-promotion");
 let isHidePromotion = false;
@@ -95,16 +121,32 @@ function random(min, max) {
 
 function floatingObject(selector, delay, size) {
   // gsap.to(요소, 시간, 옵션);
-  gsap.to(selector, //선택자
-     random(1.5, 2.5), //애니메이션 동작 시간
-  { //옵션
-    y: size,
-    repeat: -1,
-    yoyo: true,
-    ease: "power1.inOut",
-    delay: random(0, delay),
-  });
+  gsap.to(
+    selector, //선택자
+    random(1.5, 2.5), //애니메이션 동작 시간
+    {
+      //옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: "power1.inOut",
+      delay: random(0, delay),
+    }
+  );
 }
 floatingObject(".floating1", 1, 15);
 floatingObject(".floating2", 0.5, 15);
 floatingObject(".floating3", 1.5, 20);
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8,
+  })
+    .setClassToggle(spyEl, "show")
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector(".this-year");
+thisYear.textContent = new Date().getFullYear(); // 2023
