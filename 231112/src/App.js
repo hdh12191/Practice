9fdb6a4358d0149bdc11d538cb03548b2a2cd559
hdh12191 <hdh12191@naver.com>
2023-11-14 17:56:46 +0900

    Pra React

---
+++ 231112/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import "./styles.css"
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path={`${process.env.PUBLIC_URL}/movie:id`} element={<Detail />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ 231112/src/routes/Home.js
@@ -0,0 +1,46 @@
+import { useState,useEffect } from "react";
+import styles from "./Home.module.css";
+import Movie from "../components/Movie"
+
+function Home () {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div className={styles.container}>
+      {loading ? (
+        <div className={styles.loader}>
+          <span>Loading...</span>
+        </div>
+      ) : (
+        <div className={styles.movies}>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              year={movie.year}
+              coverImg={movie.medium_cover_image}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Home;
