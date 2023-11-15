b18bfe3171a66f48bf9afff92bd2727abe1abead
hdh12191 <hdh12191@naver.com>
2023-11-16 04:18:29 +0900

    pra react hooks ...ing

--- 231115/src/Hooks/useTabs.js
+++ 231115/src/Hooks/useTabs.js
@@ -1,5 +1,30 @@
 import { useState } from "react";
 
+
+// const content = [
+//   {
+//     tab: "Section 1",
+//     content: "I'm the content of the Sectioin 1",
+//   },
+//   {
+//     tab: "Section 2",
+//     content: "I'm the content of the Sectioin 2",
+//   },
+// ];
+
+// const App = () => {
+//   const { currentItem, changeItem} = useTabs(0, content);
+
+//   return (
+//     <div className="App">
+//       {content.map((section, index) => (
+//         <button onClick={() => changeItem(index)}>{section.tab}</button>
+//       ))}
+//       <div>{currentItem.content}</div>
+//     </div>
+//   );
+// };
+
 const useTabs = (initalTab, allTabs) => {
   const [currentIndex, setCurrentIndex] = useState(initalTab);
   if (!allTabs || !Array.isArray(allTabs)) {
@@ -7,7 +32,7 @@ const useTabs = (initalTab, allTabs) => {
   }
   return {
     currentItem: allTabs[currentIndex],
-    changeItem: setCurrentIndex[currentIndex]
+    changeItem: setCurrentIndex
   };
 };
 
--- 231115/src/index.js
+++ 231115/src/index.js
@@ -1,27 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom/client";
-import useTabs from "./Hooks/useTabs";
 
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Sectioin 1",
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Sectioin 2",
-  },
-];
+
 
 const App = () => {
-  const { currentItem } = useTabs(0, content);
-
+ 
   return (
-    <div className="App">
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
+    <div>
+      <h1>Hi</h1>
     </div>
   );
 };
