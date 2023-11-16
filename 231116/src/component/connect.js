cf63d90451ff136dccbc7713320c1dcf4814a0c7
hdh12191 <hdh12191@naver.com>
2023-11-17 02:55:12 +0900

    Pra Redux!

---
+++ 231116/src/component/connect.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom";	import ReactDOM from "react-dom";
+import App from "./components/App";	import App from "./components/App";
+import { Provider } from "react-redux";
+import store from "./store";
+
+
+ReactDOM.render(<App></App>, document.getElementById("root"));	ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+ 
+
+import { createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+export const addToDo = text => {
+  return {
+    type: ADD,
+    text
+  };
+};
+
+export const deleteToDo = id => {
+  return {
+    type: DELETE,
+    id
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [{ text: action.text, id: Date.now() }, ...state];
+    case DELETE:
+      return state.filter(toDo => toDo !== action.id);
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export default store;
---
+++ 231116/src/component/subscription.js
@@ -0,0 +1,29 @@
+const minus = document.getElementById("minus");
+const number = document.querySelector("span");
+
+number.innerText = 0;
+
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+  }
+};
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
+
+countStore.subscribe(onChange);
+
+const handleAdd = () => {
+  countStore.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  countStore.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
